5714eabf199a3d29a33c12fcac121c399bd20fc6
Gabriel Carneiro <gabriel.chaves.carneiro@gmail.com>
2025-10-02 14:15:43 -0300

    use domain name for efs, add vars to reduce dup code

--- scripts/setup-efs.sh
+++ scripts/setup-efs.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 # Setup EFS mounting points on EC2 instance
-# ${EFS_ID} env is set up by terraform
 
-# Install efs utils
-apt update
-apt install -y amazon-efs-utils
+EFS_DOMAIN="${EFS_ID}.efs.${REGION}.amazonaws.com"
+MOUNT_OPTS="nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
+MOUNT_POINT="/mnt/efs"
 
-# Create mounting dir and mount the instance for that session
-mkdir /mnt/efs
-mount -t efs ${EFS_ID}:/ /mnt/efs
+# Install nfs common
+sudo apt update
+sudo apt install -y nfs-common
 
-# Configure FSTAB with new mounting point for persistence
-echo "${EFS_ID}:/ /mnt/efs efs defaults,_netdev 0 0" >> /etc/fstab
+# Create mounting dir
+sudo mkdir /mnt/efs
+
+# Mount the efs instance for that session
+sudo mount -t nfs4 -o "$MOUNT_OPTS" "$EFS_DOMAIN:/" "$MOUNT_POINT"
+
+# Append mounting options to fstab for persistence
+echo "$EFS_DOMAIN:/ $MOUNT_POINT nfs4 $MOUNT_OPTS 0 0" | sudo tee -a /etc/fstab
